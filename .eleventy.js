95f1fad623a595b9769dcbc2175afb1479eb55a6
Jaydn <jaydnedwards@outlook.com>
2020-05-06 14:04:01 +0100

    Start of styling
    Created grid layout for blog and portfolio feeds, turned header and footer into partials

--- .eleventy.js
+++ .eleventy.js
@@ -2,6 +2,7 @@ module.exports = function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy("src/admin");
     eleventyConfig.addPassthroughCopy("src/_data");
     eleventyConfig.addPassthroughCopy("src/images");
+    eleventyConfig.addPassthroughCopy("src/css");
 
     return {
         dir: {
